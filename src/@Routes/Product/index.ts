ee10969869c0254150b9c427bb0ec009950e752e
akashjavalinb <139751703+akashjavalinb@users.noreply.github.com>
2024-08-28 13:12:32 +0530

    feat: add upload feat

--- src/@Routes/Product/index.ts
+++ src/@Routes/Product/index.ts
@@ -22,7 +22,7 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage, limits: { fileSize: 10000000 } });
 
 const productRoute = () => {
   const router = Router();
